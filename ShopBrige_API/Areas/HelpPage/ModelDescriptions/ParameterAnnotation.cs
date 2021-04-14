f7c58d3dcc9d4db4ac2c180a11b84ef12eefcf1d
Golda Yovan <goldayovan@outlook.com>
2021-04-14 23:40:01 +0530

    Shop Bridge API

---
+++ ShopBrige_API/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopBrige_API.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ ShopBrige_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopBrige_API.WebApiApplication" Language="C#" %>
